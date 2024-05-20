fbd7d75d5186193a12a246afbef0cfaa9b554272
SansarSharma <sansar.sharmaa@gmail.com>
2024-05-20 17:51:54 -0400

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Maze");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+
+}
+
